f33ece9805ca96041d81f0c75951696dc3cda725
ABDULHAMEED <skshaikabdulhameed786@gmail.com>
2023-12-31 20:52:58 +0530

    JWT AUTHENTICATION COMPLETED

--- week-3/04-mongo-with-jwt-auth/middleware/admin.js
+++ week-3/04-mongo-with-jwt-auth/middleware/admin.js
@@ -1,7 +1,36 @@
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require("../config.js");
+
 // Middleware for handling auth
 function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+    const token = req.headers.authorization;
+    if (!token) {
+        return res.status(401).json(
+            {
+                msg: "PLEASE PROVIDE TOKEN"
+            }
+        )
+    }
+    const sp = token.split(" ");
+    const rtoken = sp[1];
+    try {
+        const ving = jwt.verify(rtoken, JWT_SECRET);
+        if (ving) {
+            return next();
+        }
+        else {
+            res.json({
+                msg: "INVALID USER"
+            })
+        }
+    }
+    catch (error) {
+        res.status(401).json({
+            msg: "INVALID INPUTS"
+        })
+    }
 }
 
 module.exports = adminMiddleware;
